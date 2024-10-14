5aa19421440ac67fc750872d2ced095e8fb2f970
wjsw1781 <1781591279@qq.com>
2024-10-14 15:59:44 +0800

    fix

--- tools/get_issues.py
+++ tools/get_issues.py
@@ -1,7 +1,7 @@
 import requests
 import markdown
-
-
+import datetime
+# 这里优化的点逻辑 看看今天open的issue是否被comment过，如果被comment过，则不展示
 def get_open_issues(owner="wjsw1781", repo="xxx", token=None):
 
     # 如果需要身份验证，可以用token替换None
@@ -20,22 +20,41 @@ def get_open_issues(owner="wjsw1781", repo="xxx", token=None):
     response = requests.get(url, headers=headers)
 
     # 检查请求是否成功
-    if response.status_code == 200:
-        issues = response.json()
-        if not issues:
-            print("当前没有任何开放的 Issue。")
-            return []
-        else:
-            print("以下是开放的 Issue:")
-            all_issues_md=[]
-            for issue in issues:
-                # 打印标题和链接
-                print(f"- [{issue['title']}]({issue['html_url']})")
-                md_content=f'- [{issue["title"]}]({issue["html_url"]})\n\n'
-                html_content = markdown.markdown(md_content)
+    if response.status_code != 200:
+        print("当前没有任何开放的 Issue。")
+        return []
+    issues = response.json()
+    if not issues:
+        print("当前没有任何开放的 Issue。")
+        return []
+    # 过滤掉今日已经被评论的issue 没有接口 这能全部获取 然后过滤掉
+    all_open_issues_with_comments = []
+    for issue in issues:
+        comments_url = issue['comments_url']
+        comments_response = requests.get(comments_url, headers=headers)
+        comments = comments_response.json()
+        try:
+            lastest_comment = comments[-1]['created_at']                      #获取最后一条评论的时间 '2024-10-14T07:52:19Z'
+        except Exception as e:
+            continue
+        today_time=datetime.datetime.now() #获取当前时间
+        lastest_comment_time=datetime.datetime.strptime(lastest_comment,'%Y-%m-%dT%H:%M:%SZ')
+        if lastest_comment_time.date() != today_time.date():
+            all_open_issues_with_comments.append(issue)
 
-                all_issues_md.append([issue['title'],html_content])
-            return all_issues_md
-    else:
-        print(f"请求失败: {response.status_code}, {response.text}")
-        return [['请求失败', '请求失败']]
+    
+    print("以下是开放的 Issue:")
+    all_issues_md=[]
+    for issue in issues:
+        print(f"- [{issue['title']}]({issue['html_url']})")
+        md_content=f'- [{issue["title"]}]({issue["html_url"]})\n\n'
+        html_content = markdown.markdown(md_content)
+
+        all_issues_md.append([issue['title'],html_content])
+    return all_issues_md
+
+
+
+
+if __name__ == '__main__':
+    get_open_issues()
