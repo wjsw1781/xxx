8ca7566cbe7018840db3245eedb2f6e6da6f8dd6
wjsw1781 <1781591279@qq.com>
2024-10-23 10:45:07 +0800

    萨芬

---
+++ p1_vmp/p1_002_简单解释器_vm.js
@@ -0,0 +1,44 @@
+ function vmFunc(instlist) {
+    // 必须借助一个栈
+    var stack = [];
+    // 当前运行的指令索引
+    var pc = 0;
+    let right ;
+    let left ;
+
+    // 逐条执行指令
+    while (true) {
+        var inst = instlist[pc++];
+
+
+        switch (inst) {
+            case 1:
+                stack.push(instlist[pc++]);
+                break;
+            case 2:
+                right = stack.pop();
+                left = stack.pop();
+                stack.push(right * left);
+                break;
+            case 3:
+                right = stack.pop();
+                left = stack.pop();
+                stack.push(right + left);
+                break;
+            case 4:
+                console.log(stack.pop());
+                break;
+            default:
+                throw new Error("未实现的指令: " + inst);
+        }
+    }
+}
+// 示例
+// 指令含义映射
+// PUSH               1
+// multiply           2
+// add                3
+// console.log        4
+console.log(2*5+8)
+let human_complier_inslist=[1,2,1,5,2,1,8,3,4]
+vmFunc(human_complier_inslist)
